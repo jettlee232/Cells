68c7b404da11b98c94ba87ba32d7571364faab02
010101Min <suzy0455@gmail.com>
2024-05-31 13:20:35 +0900

    UI 띄우기 기본 구현
    점심 이후에 방식 바꿀 예정

--- Assets/02.Scripts/04.Scripts_StageMap/GameManager_StageMap.cs
+++ Assets/02.Scripts/04.Scripts_StageMap/GameManager_StageMap.cs
@@ -9,23 +9,39 @@ public class GameManager_StageMap : MonoBehaviour
     UnityEngine.XR.InputDevice left;
 
     private bool showUI = false;
+    private bool oldShowUI = false;
+    private GameObject[] Organelles;
 
     void Start()
     {
-        
+        Organelles = GameObject.FindGameObjectsWithTag("Organelle_SM");
     }
 
     void Update()
     {
-        
+        showOrganelleUI();
     }
 
-    private void getUp()
+    private void showOrganelleUI()
     {
         left = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
         left.TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out showUI);
 
-        if (showUI) {  }
-        else {  }
+        if (showUI && !oldShowUI)
+        {
+            foreach (GameObject organelle in Organelles)
+            {
+                organelle.GetComponent<OrganelleController_StageMap>().fShowUI();
+            }
+            oldShowUI = true;
+        }
+        if (!showUI && oldShowUI)
+        {
+            foreach (GameObject organelle in Organelles)
+            {
+                organelle.GetComponent<OrganelleController_StageMap>().fHideUI();
+            }
+            oldShowUI = false;
+        }
     }
 }
--- Assets/02.Scripts/04.Scripts_StageMap/OrganelleController_StageMap.cs
+++ Assets/02.Scripts/04.Scripts_StageMap/OrganelleController_StageMap.cs
@@ -13,14 +13,14 @@ public class OrganelleController_StageMap : MonoBehaviour
     private GameObject myUI = null;
     private GameObject player = null;
 
-    public float minScale = 0.2f; // 최소 크기
-    public float maxScale = 1f; // 최대 크기
+    public float minScale = 0.5f; // 최소 크기
+    public float maxScale = 0.7f; // 최대 크기
     public float minDistance = 1f; // 최소 거리
-    public float maxDistance = 50f; // 최대 거리
+    public float maxDistance = 20f; // 최대 거리
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).gameObject;
         StartCoroutine(cGenerateUI());
     }
 
@@ -28,39 +28,64 @@ public class OrganelleController_StageMap : MonoBehaviour
     {
         myUI = Instantiate(OC_UI, Vector3.zero, Quaternion.identity, GameObject.Find("Canvas").transform);
 
-        while (myUI = null) { yield return new WaitForSeconds(0.02f); }
+        while (myUI == null) { yield return new WaitForSeconds(0.02f); }
         myUI.transform.GetChild(0).gameObject.GetComponent<Text>().text = OC_Name;
         myUI.transform.GetChild(1).gameObject.GetComponent<Text>().text = OC_Description;
     }
 
     public void fShowUI()
     {
-        OC_UI.gameObject.SetActive(true);
+        myUI.gameObject.SetActive(true);
+        showUI = true;
         StartCoroutine(cShowUI());
     }
+    public void fHideUI() { showUI = false; }
     IEnumerator cShowUI()
     {
         while (true)
         {
             // 여기에 머 방향 돌리고 그런 거 구현해야 함
-            transform.LookAt(player.transform);
-
             RaycastHit hit;
-            float distance = Vector3.Distance(transform.position, player.transform.position);
-            OC_UI.transform.position = Camera.main.WorldToScreenPoint(this.gameObject.transform.position);
-            if ((distance >= 50f) || (Physics.Raycast(player.transform.position, (this.transform.position - player.transform.position).normalized, out hit, distance))) { OC_UI.gameObject.SetActive(false); }
+            Vector3 closest = this.gameObject.GetComponent<Collider>().ClosestPoint(player.transform.position);
+            Vector3 vDistance = (closest - player.transform.position);
+            Vector3 vNDistance = vDistance.normalized;
+            float fDistance = vDistance.magnitude;
+            bool tooClose = false;
+            
+            //Vector3.up * (myUI.GetComponent<RectTransform>().rect.height)
+            //if ((distance >= 50f) || (Physics.Raycast(player.transform.position, (this.transform.position - player.transform.position).normalized, out hit, distance))) { myUI.gameObject.SetActive(false); }
+            if (fDistance < 1f) { tooClose = true; }
+            if (fDistance >= 20f) { myUI.gameObject.SetActive(false); }
             else
             {
-                OC_UI.gameObject.SetActive(true);
-                float scale = Mathf.Clamp(1 - (distance / maxDistance), minScale, maxScale);
-                OC_UI.gameObject.transform.localScale = new Vector3(scale, scale, scale);
+                float scale = 0f;
+                myUI.gameObject.SetActive(true);
+                if (tooClose) { scale = 0.005f * maxScale; }
+                else { scale = (Mathf.Clamp(1 - (fDistance / maxDistance), minScale, maxScale)) * 0.005f; }
+                myUI.gameObject.transform.localScale = new Vector3(scale, scale, scale);
             }
 
+            if (tooClose)
+            {
+                myUI.transform.position = closest + (GetLocalYAxis(vNDistance) * (myUI.GetComponent<RectTransform>().rect.height) * (myUI.gameObject.transform.localScale.x));
+            }
+            else { myUI.transform.position = closest - vNDistance + (GetLocalYAxis(vNDistance) * (myUI.GetComponent<RectTransform>().rect.height) * (myUI.gameObject.transform.localScale.x)); }
 
+            myUI.transform.LookAt(player.transform);
+            myUI.transform.Rotate(0, 180, 0);
 
             if (showUI == false) { break; }
             yield return new WaitForSeconds(0.02f);
         }
-        OC_UI.gameObject.SetActive(false);
+        myUI.gameObject.SetActive(false);
+    }
+
+    private Vector3 GetLocalYAxis(Vector3 dir)
+    {
+        Vector3 right = Vector3.Cross(dir, Vector3.up);
+        if (right == Vector3.zero) { right = Vector3.Cross(dir, Vector3.forward); }
+        Vector3 localUp = Vector3.Cross(right, dir);
+
+        return localUp.normalized;
     }
 }
